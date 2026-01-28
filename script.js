ef415b7b0083effdcdb0c21971ff1a5f9d9e4df1
kanayawiiii <aapaya74@gmail.com>
2026-01-28 09:00:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,269 @@
+const students = [
+   {
+    "id": "a936a301-67a0-4074-9edf-f6c7e987b3e6",
+    "nis": "13508",
+    "name": "Anandhita Syakira Hidayat",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "222f794d-2628-499f-a18a-584c4a8c09aa",
+    "nis": "13518",
+    "name": "Anisa Mulia",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "d0ac9cf0-6c64-41ec-a4b2-204f3ead8362",
+    "nis": "13523",
+    "name": "Aqbiel Juand Dwirika Rizki",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "4ceda83b-b05f-450c-a427-ab2797abdac3",
+    "nis": "13528",
+    "name": "Aries Indra Prakoso",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "9d90b45a-a4e7-4f5c-8371-60c45852c730",
+    "nis": "13561",
+    "name": "Bernessa Rahadatul'aisy",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "a6216cd0-dfc5-4a7f-9e8a-003b723ff389",
+    "nis": "13596",
+    "name": "Dhea Ananda",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "33b65128-c5aa-4d0e-b060-2faea9ebc305",
+    "nis": "13597",
+    "name": "Dhea Lucita",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "9ce46f6a-3aee-4d8e-9d95-a4eb98f4f031",
+    "nis": "13627",
+    "name": "Fandy Fitriyanto",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "aecf06e7-cc70-47b6-80eb-9671f68903d1",
+    "nis": "13637",
+    "name": "Farius Cinta ",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "a1265a96-642d-4ca6-9542-26ed114918e6",
+    "nis": "13640",
+    "name": "Fatih Mutaqqin",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "5e5df2d4-b2e0-4bd7-ab9a-39f59504f994",
+    "nis": "13642",
+    "name": "Febrian Imanuel Heti Pratama",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "6dec2c18-8572-42d8-b948-2736a19dc92e",
+    "nis": "13642",
+    "name": "Gerald Arvin",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "ddd6adf3-c98f-4023-9b9d-5588f086e07d",
+    "nis": "13658",
+    "name": "Grevaldio Putra",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "a800deb4-e3a4-45f7-b80f-597e2a20b22c",
+    "nis": "13659",
+    "name": "Hafidz Khaizuran",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "ace433d3-0e70-4760-b09a-d512c9581648",
+    "nis": "13660",
+    "name": "Hana Aantikah",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "fcb4268b-b8e8-45e5-9c2e-150d90ffbdfc",
+    "nis": "13667",
+    "name": "Inggrid Nikita",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "64634a93-f651-45e6-903e-aa4276e48c8e",
+    "nis": "13680",
+    "name": "Jethro Ata",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "fe8c5f99-26a9-48f9-8c1e-35f78e14ad5b",
+    "nis": "13683",
+    "name": "Joy Abimanyu",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "037ed1f8-170f-4e4c-a1f3-3e56d5411b28",
+    "nis": "13684",
+    "name": "Kafka Xavier",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "ece758d9-2fb0-49d7-8b34-ca1ab74dffdc",
+    "nis": "13685",
+    "name": "Kanaya Karin",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "cecc17ba-fe10-4909-b697-6bbf92309815",
+    "nis": "13686",
+    "name": "Karin Vanessa",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "41d260f9-fbe6-4b5a-906c-3b31e41691eb",
+    "nis": "13693",
+    "name": "Kenzie Hafiz",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "9b454f27-aa0f-450c-bb67-aa6a11069215",
+    "nis": "13739",
+    "name": "Monica Dewi",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "ca881ce9-42cf-43fa-a47c-8065892030f1",
+    "nis": "13718",
+    "name": "Muchammad Fadhil Toriq",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "d99cb0f6-26c3-48af-a819-de5b5b5d0d28",
+    "nis": "13742",
+    "name": "Muhammad akhtar",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "99c6717e-7cbd-4bfa-9c9b-4873b4eb696c",
+    "nis": "13745",
+    "name": "Muhammad Brahmana",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "12f3e84b-be7b-4f44-b474-3614b9201c13",
+    "nis": "13748",
+    "name": "Muhammad Fadhil Wafi",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "a4b71e5e-4d19-41a9-80f2-273ecef95e5a",
+    "nis": "13754",
+    "name": "Muhammad Ikhsan ",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "153e1650-67f8-4351-b052-429edb42b1c8",
+    "nis": "13758",
+    "name": "Muhammad Riko",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "85cb1faf-13ca-4c74-91f2-7e5013b29ff6",
+    "nis": "13763",
+    "name": "Mutiara Saniyyah",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "5d5e868d-3ef1-4d1a-ac7e-90ba417bcc09",
+    "nis": "13768",
+    "name": "Nadhief Fathan",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "0b2dca23-59a8-45f6-8d40-32d869ed3e80",
+    "nis": "13777",
+    "name": "Nafisah Dzatir",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "179b359e-0c5a-429c-9aea-5beae0ca3632",
+    "nis": "13824",
+    "name": "Rafasyah",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "c4db2732-700e-4731-b332-1e7a0cf2b273",
+    "nis": "13841",
+    "name": "Reza Emmeraldi",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "cbd0bd75-c93f-4e90-9c44-ee73dd3fc466",
+    "nis": "13885",
+    "name": "Velisa Khalilah",
+    "class_id": "XI-C1",
+    "created_at": null
+  },
+  {
+    "id": "9a81d6d1-6edf-4794-ad93-eff8625e72f7",
+    "nis": "13895",
+    "name": "Zaki Rahmat",
+    "class_id": "XI-C1",
+    "created_at": null
+  }
+];
+
+const tbody = document.getElementById("data-siswa");
+
+students.forEach((student, index) => {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${student.nis}</td>
+    <td>${student.name}</td>
+    <td>${student.class_id}</td>
+  `;
+
+  tbody.appendChild(row);
+});
